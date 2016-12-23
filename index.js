cfcdfbd1f64a8c54dcac2fa555ef05543cf59ff3
Per Guth <mail@perguth.de>
2016-12-23 14:48:45 +0100

    work in absence of optional parameters

--- index.js
+++ index.js
@@ -9,15 +9,16 @@ if (global.flipTape && global.flipTape.tapeMock) {
 
 String.prototype.test = function (arg1, cb) { // eslint-disable-line
   var msg = this.toString()
-  return flipTape(msg, arg1, cb, test)
+  return flipTape(msg, arg1, cb, null, null, test)
 }
 
 String.prototype.only = function (arg1, cb) { // eslint-disable-line
   var msg = this.toString()
-  return flipTape(msg, arg1, cb, test.only)
+  return flipTape(msg, arg1, cb, null, null, test.only)
 }
 
-function flipTape (arg0, arg1, cb, tape) {
+function flipTape (arg0, arg1, cb, _, __, tape) {
+  // arg0, arg1 are optional
   if (!tape) tape = test
   cb = cb || arg1 || arg0
   return tape(arg0, arg1, testObject => {
